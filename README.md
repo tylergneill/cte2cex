52d48f01ea4133c1dba6fe3c6a930d9dc03204c8
Tyler Neill <tyler.g.neill@gmail.com>
2020-06-19 16:11:09 -0400

    update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cte2cex
 
-A tool for converting multiple Classical Text Editor (CTE) files (transcripts, editions) to a CEX library file for use in Brucheion.
+A tool for converting multiple Classical Text Editor (CTE) and/or plain-text files (transcripts, editions) to a CEX library file for use in Brucheion.
 
 Requirements:
 
@@ -8,19 +8,29 @@ Python 2.7 + libraries: re, json, subprocess, webbrowser, tqdm
 
 Instructions:
 
-1. Review "input\_guidelines.md"; optionally test compliance of individual input files ahead of time with "validate.py".
+1. Make sure input data is properly formatted.
 
-2. For one-time test use: 
+* Review "input\_guidelines.md". 
+* Optionally test compliance of individual input files for compatibility with "regex\_replacements.py" and project config file ahead of time with "validation.py".
 
-* Manually place desired CTE files in folder "input\_cte\_files".
-* Run cte2cex.py; optionally supply '-v' flag for run-time input validation.
-* Collect output CEX from "output\_cex\_files".
+2. Create project JSON file.
+
+* Choose project name for output filename.
+* Supply static paths for individual input (e.g. CTE) files as well as output path (e.g., `Brucheion\cex`.
+* Prepare statis CEX catalog entries.
+* Update CTS protocol components as needed (see [Homer Multitext](https://www.homermultitext.org/hmt-doc/cite/texts/ctsoverview.html) and [cite-architecture.org](http://cite-architecture.org/cts/)).
+* Choose options for handling of zero-space word separator, additional negative images, orthography normalization, and so on.
 
 3. For recurring project use:
 
-* Create project JSON file: Supply static paths to individual CTE files and corresponding CEX catalog entries. Also supply desired filename and destination path for CEX output (e.g., .../Brucheion/cex). For new projects, update CTS protocol components as needed (see [Homer Multitext](https://www.homermultitext.org/hmt-doc/cite/texts/ctsoverview.html) and [cite-architecture.org](http://cite-architecture.org/cts/)). Choose options for handling of zero-space word separator, additional negative images, and so on.
-* Run Brucheion and log in with desired "username".
-(Note: usernames utilized here for projects, not people.)
-* Run cte2cex.py with '-bp' flag to update Brucheion project.
-(Note: contents of folder "input\_cte\_files" will be purged! Also, any preexisting CEX with same filename in destination path will be renamed with "_bkup".)
-* Wait briefly until processing completes and Brucheion confirms in-browser successful execution of "load" API call. (Note: All conflicting Brucheion data in current "user" database will be overwritten. Orthography normalization will need to be re-run.)
+* Start Brucheion and log into desired project ("username").
+* Run cte2cex.py, specifying project json file and '-u' flag as arguments; optionally supply '-v' flag for run-time input validation.
+> Note: With '-u' flag, contents of folder "tmp\input\_cte\_files" will be purged! Also, any preexisting CEX with same filename in output path will be renamed with "_bkup". Most importantly, all conflicting Brucheion data in the current "user" database will be overwritten.
+* Observe progress in command line window, wait for confirmation of success in loading (and normalizing, if applicable) in a new tab in the browser.
+
+4. For one-time test use:
+
+* Manually place desired input files in input folder.
+* Run cte2cex.py *without* the '-u' flag; optionally supply '-v' flag for run-time input validation.
+* Collect output CEX from "tmp\output\_cex\_files".
+> Note: Nothing more will be automatically done with the resulting output file.
--- regex_replacements.py
+++ regex_replacements.py
@@ -5,58 +5,57 @@ regex_replacements = [
 
 	# 0: eliminate unused material; light standardization
 	[
-		(r'\xef\xbb\xbf',''), 					# BOM
-		(r'\r\n', '\n'), 						# Windows-style line break
+		(r'\xef\xbb\xbf',''),	# BOM
+		(r'\r\n', '\n'),	# Windows- > Linux-style line break
 
-		(r'CTE_[\s\S]*?{Text\\', ''), 			# CTE header
-		(r'\\Text}[\s\S]*', ''), 				# CTE footer
+		(r'CTE_[\s\S]*?{Text\\', ''),	# CTE header
+		(r'\\Text}[\s\S]*', ''),	# CTE footer
 
-		(r'(({F[^\\]*?\\)|(\\F}))', ''), 		# CTE text formatting markup
-		(r'{P\\[^\\]*?\\P}', ''), 				# CTE paragraph formatting markup
+		(r'(({F[^\\]*?\\)|(\\F}))', ''),	# CTE text formatting markup
+		(r'{P\\[^\\]*?\\P}', ''),	# CTE paragraph formatting markup
 		(r'{C(N1)?\\([^\\]*?)\\C}', '{\\2}'), 	# CTE passage ("chapter") identifier markup
 		(r'{E6[^\\]*?\\([^\\]*?)\\E}', '\\1'), 	# CTE hyperlink markup (e.g. Dropbox links)
-		(r'{S\\[^\\]*?\\S}', ''),				# CTE special line break
+		(r'{S\\[^\\]*?\\S}', ''),	# CTE special line break
 
-		(r'^Transcript.*', ''), 				# human-readable note
-		(r'^Critical.*', ''), 					# human-readable note
-		(r'^\^\[Sources.*', ''), 				# human-readable note
-		(r'^[^\n]*?missing.*\n', ''),			# human-readable note
-		(r'\^\[[pnv](?!(\^\]))[\s\S]*?\^\]', ''), # human-readable note
+		(r'^Transcript.*', ''),	# human-readable header note
+		(r'^Critical.*', ''),	# human-readable header note
+		(r'^\^\[Sources.*', ''),	# human-readable header note
+		(r'^[^\n]*?missing.*\n', ''),	# human-readable note mid-transcript
+		(r'\^\[[pnv](?!(\^\]))[\s\S]*?\^\]', ''), # human-readable note mid-transcript
 
-		(r'\^\"', '^'), 						# escaped half-punctuation mark ^
-		(r'\^\+', '-'), 						# escaped hyphen
-		(r'–', '_'),							# n-dash
+		(r'\^\"', '^'),	# escaped half-punctuation mark ^
+		(r'\^\+', '-'),	# escaped hyphen
+		(r'–', '_'),	# n-dash
 
-		(r'\t', ''),							# tab
-		(r'\n{2,}', '\n'), 						# multiple line breaks
-		(r'\A\s*', ''), 						# file-initial whitespace
+		(r'\t', ''),	# tab
+		(r'\n{2,}', '\n'),	# multiple line breaks
+		(r'\A\s*', ''),	# file-initial whitespace
 
 		(r'\A(\(%s_\d+[^\)]*?\))([^{]{0,250})({([^}]*?)})', '\\3\\1\\2'),
-		(r'(\(%s_\d+[^\)]*?\))({([^}]*?)})', '\\2\\1'),
-												# object (folio) identifer templates
+		(r'(\(%s_\d+[^\)]*?\))({([^}]*?)})', '\\2\\1'), # object (folio) identifer templates
 
-		(r'\n{2,}', '\n'), 						# multiple line breaks
-		(r'\s*\Z', ''), 						# file-final whitespace
-		(r'^ *', ''),							# line-initial space
+		(r'\n{2,}', '\n'),	# multiple line breaks
+		(r'\s*\Z', ''),	# file-final whitespace
+		(r'^ *', ''),	# line-initial space
 	],
 
-	# 1: simplify to three element types for Brucheion relations
+	# 1: simplify to three element types for CEX !relations block
 	[
-		(r'(\(?{[^}]*?}\)?)', '\n\\1\n'),		# passage identifiers
+		(r'(\(?{[^}]*?}\)?)', '\n\\1\n'),	# passage identifiers
 		(r'(\(%s_\d+[^\)]*?[rv]?)(,?\d+?\))', '\n\\1)\n'),	# object identifiers
-		(r'^\s*', ''),							# line-initial whitespace
+		(r'^\s*', ''),	# line-initial whitespace
 		(r'^(?!(\(%s_\d+[^\)]*?\))|({[^}]*?})).+', '...'),	# specific content details
-		(r'(\n\.\.\.){2,}', '\\1'),				# consecutive lines of content
-		(r'([rv])\d+\)', '\\1)'),				# object (folio) identifer line numbers
+		(r'(\n\.\.\.){2,}', '\\1'),	# consecutive lines of content
+		(r'([rv])\d+\)', '\\1)'),	# object (folio) identifer line numbers
 		
 	],
 
-	# 2: format textual content for Brucheion ctsdata
+	# 2: format textual content for CEX !ctsdata block
 	[
-		(r'\n', '-NEWLINE-'), 					# encoding of newline
+		(r'\n', '-NEWLINE-'),	# encoding of newline
 		(r'(-NEWLINE-)?{([^}]*?)}', '\n\\2\t'),	# two columns: passage identifier, rest
-		(r'\((%s_\d+[^\)]*?)\)', '{\\1}'), 		# encoding of in-line object identifer
-		(r'^\s*', ''), 							# line-initial whitespace
+		(r'\((%s_\d+[^\)]*?)\)', '{\\1}'),	# encoding of in-line object identifer
+		(r'^\s*', ''),	# line-initial whitespace
 	],
 
 ]
